78bf5cfa33c207b41c16b8c3cb29f9be2fc2ec63
John Herrlin <john.herrlin@fortnox.se>
2022-06-01 12:57:37 +0200

    7th fix

--- src/chords.clj
+++ src/chords.clj
@@ -1,105 +1,71 @@
 (ns chords)
 
 (def tones [:c :c# :d :d# :e :f :f# :g :g# :a :a# :b])
-(count tones) ;; => 12
 
-(defn tone
-  ([n]
-   (tone n identity))
-  ([n f]
-   (fn [tones]
-     (nth tones (mod (f n) 12)))))
-
-(def prim    (partial tone 0))
-(def sekund  (partial tone 1))
-(def ters    (partial tone 2))
-(def kvart   (partial tone 3))
-(def kvint   (partial tone 4))
-(def sext    (partial tone 5))
-(def septima (partial tone 6))
-
-(def flat dec)
-
-((prim) tones)
-;; => :c
-((prim flat) tones)
-;; => :b
-
-(doseq [n (range 12)]
-  (eval
-   `(def ~(symbol (str "f" n)) (partial tone ~n))))
+(def prim    #(nth % 0))
+(def sekund  #(nth % 1))
+(def ters    #(nth % 2))
+(def kvart   #(nth % 3))
+(def kvint   #(nth % 4))
+(def sext    #(nth % 5))
+(def septima #(nth % 6))
 
 (defn find-root
-  ""
-  [root tones]
-  {:pre [((set tones) root)]}
+  [tone tones]
+  {:pre [((set tones) tone)]}
   (->> (cycle tones)
-       (drop-while #(not= % root))
+       (drop-while #(not= % tone))
        (take 12)
        (vec)))
 
-(defn succ [tones note]
-  (let [idx (.indexOf tones note)]
-    (nth tones (inc idx))))
+(defn pred [x xs]
+  {:pre [((set xs) x)]}
+  (->> (reverse xs)
+       (cycle)
+       (drop-while #(not= % x))
+       (second)))
 
-(defn pred [tones note]
-  (let [idx (.indexOf tones note)]
-    (nth tones (dec idx))))
+(defn succ [x xs]
+  {:pre [((set xs) x)]}
+  (->> xs
+       (cycle)
+       (drop-while #(not= % x))
+       (second)))
 
-(defn chord [tones root major-or-minor & [color]]
+(def sharp succ)
+(def flat  pred)
 
-  (let [f (apply comp fns)]
-    (f (find-root root tones))))
+(let [t (fn [x] (fn [xs] (nth xs x)))]
+  ;;                   Hel   Hel   Halv  Hel   Hel   Hel
+  (def major (juxt (t 0) (t 2) (t 4) (t 5) (t 7) (t 9) (t 11)))
 
-(defn major )
+  ;;                   Hel   Halv  Hel   Hel   Halv  Hel
+  (def minor (juxt (t 0) (t 2) (t 3) (t 5) (t 7) (t 8) (t 10)))
 
-(chord tones :c major)
-(chord tones :c major seven)
-(chord tones :c major maj-seven)
-(chord tones :c minor seven)
-(chord tones :c minor maj-seven)
+  (def triad (juxt (t 0) (t 2) (t 4)))
+  )
 
-(defn minor-steps [tones]
-  [(nth tones 0)  ;; Hel
-   (nth tones 2)  ;; Halv
-   (nth tones 3)  ;; Hel
-   (nth tones 5)  ;; Hel
-   (nth tones 7)  ;; Halv
-   (nth tones 8)  ;; Hel
-   (nth tones 10) ;; Hel
-   ])
+(defn chord-p [tones root major-or-minor & [color]]
+  (let [color' (if color
+                 (partial color tones (major tones))
+                 identity)]
+    (->> tones (find-root root) major-or-minor triad color')))
 
-(defn major-steps [tones]
-  [(nth tones 0)  ;; Hel
-   (nth tones 2)  ;; Hel
-   (nth tones 4)  ;; Halv
-   (nth tones 5)  ;; Hel
-   (nth tones 7)  ;; Hel
-   (nth tones 9)  ;; Hel
-   (nth tones 10) ;; Halv
-   ])
+(def chord (partial chord-p tones))
 
-(find-root :c tones)
+(defn seven [all-tones scale-tones triad]
+  (conj triad (-> (septima scale-tones) (flat all-tones))))
 
-(def p partial)
-
-(defmacro chord [])
-
-(def major         (juxt (prim) (ters) (kvint)))
-(def major-seventh (juxt (prim) (ters) (kvint) (septima flat)))
-
-(major (find-root :c tones))
+(defn maj-seven [all-tones scale-tones triad]
+  (conj triad (septima scale-tones)))
 
 
+(minor tones) ;; => [:c :d :d# :f :g :g# :a#]
+(major tones) ;; => [:c :d :e  :f :g :a  :b]
 
-
-
-(find-root :e tones)
-;; => (:e :f :f# :g :g# :a :a# :b :c :c# :d :d#)
-
-(chord tones :e identity)
-;; => (:e :f :f# :g :g# :a :a# :b :c :c# :d :d#)
-(chord tones :e major)
-;; => [:e :g# :b]
-(chord tones :e minor)
-;; => [:e :g :b]
+(chord :c major)            ;; => [:c :e :g]
+(chord :c major seven)      ;; => [:c :e :g :a#]
+(chord :c major maj-seven)  ;; => [:c :e :g :b]
+(chord :c minor)            ;; => [:c :d# :g]
+(chord :c minor seven)      ;; => [:c :d# :g :a#]
+(chord :c minor maj-seven)  ;; => [:c :d# :g :b]
