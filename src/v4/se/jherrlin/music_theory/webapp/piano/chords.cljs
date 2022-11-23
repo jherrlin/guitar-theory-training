0720401cdb57afbd03c613a85360cc044a73066c
John Herrlin <jherrlin@gmail.com>
2022-11-23 10:20:08 +0100

    v4 piano wip

--- src/v4/se/jherrlin/music_theory/webapp/piano/chords.cljs
+++ src/v4/se/jherrlin/music_theory/webapp/piano/chords.cljs
@@ -8,7 +8,9 @@
    [v4.se.jherrlin.music-theory.definitions :as definitions]
    [v4.se.jherrlin.music-theory.intervals :as intervals]
    [v4.se.jherrlin.music-theory.utils :as utils]
-   [v4.se.jherrlin.music-theory.webapp.piano.view :as piano.view]))
+   [v4.se.jherrlin.music-theory.webapp.piano.view :as piano.view]
+   [se.jherrlin.utils
+    :refer [fformat rotate-until]]))
 
 
 
@@ -18,6 +20,7 @@
     :s (fn [db [n']] (get db n' :tone))}
    {:n ::as-intervals
     :s (fn [db [n']] (get db n' false))}
+   {:n ::nr-of-octavs}
    {:n ::path-params}
    {:n ::query-params}])
 
@@ -35,14 +38,19 @@
         as-intervals @(re-frame/subscribe [::as-intervals])
         key-of       @(re-frame/subscribe [:key-of])
         path-params  @(re-frame/subscribe [::path-params])
-        query-params @(re-frame/subscribe [::query-params])]
+        query-params @(re-frame/subscribe [::query-params])
+        nr-of-octavs @(re-frame/subscribe [::nr-of-octavs])]
     (when (and chord key-of)
       (let [{id          :chord/id
              indexes     :chord/indexes
              intervals   :chord/intervals
              explanation :chord/explanation
              sufix       :chord/sufix}
-            (get @definitions/chords @(re-frame/subscribe [::chord]))]
+            (get @definitions/chords @(re-frame/subscribe [::chord]))
+            tones (utils/tones-by-key-and-intervals
+                   utils/all-tones
+                   key-of
+                   intervals)]
         [:div
 
          ;; Links to keys
@@ -76,17 +84,73 @@
                {:disabled (= id chord)}
                (str (or display-text sufix))]]])]
 
+         ;; Highlight tones
+         [:div {:style {:margin-top  "1em"
+                        :display     "flex"
+                        :align-items "center"}}
 
-         [piano.view/piano
-         (if as-intervals
-           :interval
-           :tone)
-         (utils/tones-by-key-and-intervals-2
-          utils/all-tones
-          key-of
-          intervals)]
-         ]
-        ))))
+          (for [{:keys [tone match?]}
+                (let [tones-set (set tones)]
+                  (->> utils/all-tones
+                       (rotate-until #(% key-of))
+                       (map (fn [tone]
+                              (cond-> {:tone tone}
+                                (seq (set/intersection tones-set tone))
+                                (assoc :match? true))))))]
+            ^{:key (str tone "something")}
+            [:div {:style {:width     "4.5em"
+                           :font-size "0.9em"}}
+             (for [t' (sort-by (fn [x]
+                                 (let [x (str x)]
+                                   (cond
+                                     (and (= (count x) 3) (str/includes? x "#"))
+                                     1
+                                     (= (count x) 3)
+                                     2
+                                     :else 0))) tone)]
+               ^{:key (str tone t')}
+               [:div {:style {:margin-top "0em"}}
+                [:div
+                 {:style {:color       (if-not match? "grey")
+                          :font-weight (if match? "bold")}}
+                 (-> t' name str/capitalize)]])])]
+
+         ;; Chord name
+         [:div {:style {:margin-top "1em"
+                        :height     "100%"
+                        :display    "inline-flex"}}
+          [:h2 (str (-> key-of name str/capitalize) sufix)]
+          (when explanation
+            [:p {:style {:margin-left "4em"
+                         :margin-top  "0.5em"}}
+             (str "(" explanation ")")])]
+
+         ;; Intervals
+         [:pre {:style {:overflow-x "auto"}}
+          (->> (map
+                (fn [interval index]
+                  (str (fformat "%8s" interval) " -> " (-> index name str/capitalize)))
+                intervals
+                tones)
+               (str/join "\n")
+               (apply str)
+               (str "Interval -> Tone\n"))]
+
+
+         [:br]
+
+         [:div {:style {:display :flex}}
+          (for [index (range 0 nr-of-octavs)]
+            ^{:key (str "piano-octav-index-" index)}
+            [piano.view/piano
+             (if as-intervals
+               :interval
+               :tone)
+             (utils/tones-by-key-and-intervals-2
+              utils/all-tones
+              key-of
+              intervals)]
+            )]]))))
 
 (def routes
   [["/v4/piano/chord/:key-of/:chord-name"
@@ -94,16 +158,20 @@
      :view [chords-view]
      :controllers
      [{:parameters {:path  [:key-of :chord-name]
-                    :query [:as-intervalls]}
-       :start      (fn [{{:keys [key-of chord-name] :as p} :path
-                         {:keys [as-intervalls] :as q}     :query}]
+                    :query [:as-intervalls :octavs]}
+       :start      (fn [{{:keys [key-of chord-name] :as p}    :path
+                         {:keys [as-intervalls octavs] :as q} :query}]
                      (js/console.log "path params" p)
                      (js/console.log "query params" q)
                      (re-frame/dispatch [::path-params p])
                      (re-frame/dispatch [::query-params q])
-                     (let [key-of (-> key-of
+                     (let [octavs (if-not octavs
+                                    2
+                                    (js/parseInt octavs))
+                           key-of (-> key-of
                                       (str/lower-case)
                                       (str/replace "sharp" "#"))]
+                       (re-frame/dispatch [::nr-of-octavs octavs])
                        (re-frame/dispatch [:key-of (keyword key-of)])
                        (re-frame/dispatch [::chord (keyword chord-name)])
                        (re-frame/dispatch [::as-intervals (boolean (seq as-intervalls))])))}]}]])
